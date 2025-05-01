e463b45a70a1529542b9c0cb5464a5a9a6a452f5
HexDevourSoap <forgottenbabo@gmail.com>
2025-05-01 03:22:01 +0300

    fields written

--- cypress/e2e/demoqa.cy.js
+++ cypress/e2e/demoqa.cy.js
@@ -1,12 +1,41 @@
-import { HomePag } from "../pageObjects/HomePage"
+import { HomePage } from "../pageObjects/HomePage"
 describe('template spec', () => {
   beforeEach(() => {
     cy.visit('https://demoqa.com/automation-practice-form')
   })
 
-  it('Make an Appointment', () => {
+  it('Finaltask', () => {
+    HomePage.firstnameField.type('John')
+    HomePage.lastnameField.type('Doe')
+
+    HomePage.emailField.type("demo@gmail.com");
+    HomePage.genderField.click();
+    HomePage.mobileField.type("3513456789");
+
+    HomePage.dateOfBirthField.click();
+    HomePage.dateofBirthYear.select("1990");
+    HomePage.dateofBirthMonth.select("October");
+    HomePage.dateofBirthDay.click();
+    
+    HomePage.hobbyButton.click();
+    
+    HomePage.subjectField.type('Economics');
+    HomePage.subjectFieldChoice('Economics').click();
+
+    
+
+    //HomePage.uploadPic.click();
 
 
+     HomePage.addressField.type("123 Main St, Springfield, USA");
+     HomePage.stateButton.type("NCR");
+     HomePage.stateButtonChoice.click();
+    
+     HomePage.cityButton.type("Delhi");
+     HomePage.cityButtonChoice.click(); 
+      
+     HomePage.submitButton.click();
+    
   });
 
 
--- cypress/pageObjects/HomePage.js
+++ cypress/pageObjects/HomePage.js
@@ -3,15 +3,78 @@ export class HomePage extends BasePage {
     static get url() {
         return "/#/";
     }
+    static get firstnameField() {
+        return cy.get("#firstName");
+      }
+    
+    static get lastnameField() {
+        return cy.get("#lastName");
+      }
 
+      static get emailField(){
+        return cy.get("#userEmail");
+      }
+        static get genderField(){
+            return cy.get("#genterWrapper > div.col-md-9.col-sm-12 > div:nth-child(1) > label");
+        } 
+        static get mobileField(){
+            return cy.get("#userNumber");
+        }  
+      
+        static get dateOfBirthField(){
+            return cy.get("#dateOfBirthInput");
+        }
+        static get dateofBirthYear(){
+            return cy.get("#dateOfBirth > div.react-datepicker__tab-loop > div.react-datepicker-popper > div > div > div.react-datepicker__month-container > div.react-datepicker__header > div.react-datepicker__header__dropdown.react-datepicker__header__dropdown--select > div.react-datepicker__year-dropdown-container.react-datepicker__year-dropdown-container--select > select");
+        }
+        static get dateofBirthMonth(){
+            return cy.get("#dateOfBirth > div.react-datepicker__tab-loop > div.react-datepicker-popper > div > div > div.react-datepicker__month-container > div.react-datepicker__header > div.react-datepicker__header__dropdown.react-datepicker__header__dropdown--select > div.react-datepicker__month-dropdown-container.react-datepicker__month-dropdown-container--select > select");
+        }
+        static get dateofBirthDay(){
+            return cy.get("#dateOfBirth > div.react-datepicker__tab-loop > div.react-datepicker-popper > div > div > div.react-datepicker__month-container > div.react-datepicker__month > div:nth-child(1) > div.react-datepicker__day.react-datepicker__day--004");
+        }
+        static get subjectField(){
+            return cy.get("#subjectsContainer input");
+        }
+        static subjectFieldChoice(Economics) {
+            return cy.get('.subjects-auto-complete__menu').contains(Economics);
+          }
+        static get closeLardge(){
+            return cy.get("#closeLargeModal");
+        }
 
+        static get hobbyButton(){
+            return cy.get("#hobbiesWrapper > div.col-md-9.col-sm-12 > div:nth-child(3) > label");
+        }
 
+        static get uploadPic(){
+            return cy.get("#uploadPicture");
+        }
 
+        static get stateButton(){
+            return cy.get('#state > div > div.css-1hwfws3 > div.css-1wa3eu0-placeholder');
+        }
+        
+        static get stateButtonChoice(){
+            return cy.get('#react-select-3-option-0');
+        }
+        static get cityButton(){
+            return cy.get('#city > div > div.css-1hwfws3 > div.css-1wa3eu0-placeholder');
+        }
+        static get cityButtonChoice(){
+            return cy.get('#react-select-4-option-0');
+        }
 
+        static get addressField(){
+            return cy.get("#currentAddress");
+        }
+        static get submitButton(){
+            return cy.get("#submit");
+        }
 
-
-
-
+        static get closeButton(){
+            return cy.get("#closeLargeModal");
+        }
 
 
 
